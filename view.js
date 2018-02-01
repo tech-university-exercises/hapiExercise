37970d14f26dd46b7ef2ba5824e04dee5cd4471f
Anmol varma <anmol_varma@github.com>
2018-02-01 16:34:00 +0530

    view setup and statuscode 200 recieved

--- view.js
+++ view.js
@@ -6,7 +6,7 @@ const handle = require('handlebars');
 const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
-  port: process.argv[2] || 8080,
+  port: 8080,
 });
 
 server.register(Vision, (err) => {
@@ -14,7 +14,7 @@ server.register(Vision, (err) => {
 });
 
 server.views({ engines: { html: handle }, path: Path.join(__dirname, 'templates') });
-server.route({ path: '/{name?}', method: 'GET', handler: { view: 'index.html' } });
+server.route({ path: '/', method: 'GET', handler: { view: 'index.html' } });
 
 server.start(() => {
   console.log('Server running at:', server.info.uri);
